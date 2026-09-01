1f8429213013e779a7231f8256383584f20b0fd9
yogesh6007 <yogeshparmar6007@gmail.com>
2026-09-01 16:52:40 +0530

    Delete unnecessary files

--- connector-frontend/src/pages/organizer/Messages.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Messages from '../student/Messages';
-
-export default function OrganizerMessages() {
-  return <Messages />;
-}
--- connector-frontend/src/pages/organizer/Notifications.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Notifications from '../student/Notifications';
-
-export default function OrganizerNotifications() {
-  return <Notifications />;
-}
